75b21a9bb9d0870822a86b376c78adcacafc4e78
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2023-07-26 00:19:01 +0530

    Create 71A.cpp

---
+++ 71A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+void WTL(){
+    string s;
+    cin>>s;
+    int i;
+    for(i=0;s[i]!='\0';i++){
+        
+    }
+    i--;
+    cout<<s[o]<<i-2<<s[i];
+}
+int main()
+{
+    int n;
+    cin>>n;
+    while(n>0)
+    {
+        WTL();
+        n--;
+    }
+
+    return 0;
+}
